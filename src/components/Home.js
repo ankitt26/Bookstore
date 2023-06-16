f6806b0bf4ddae5998977ee2ec2d2613c9597d54
Ankit <ankit26kushwa@gmail.comm>
2023-06-16 11:00:28 +0530

    Add cases in extra reducer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,37 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import AddNewBook from './AddNewBook';
-import Book from './Book';
-import { Fetchdata } from '../redux/books/Fetch';
+// import Book from './Book';
+import { fetchData } from '../redux/books/Fetch';
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(Fetchdata());
+    dispatch(fetchData());
   }, [dispatch]);
 
-  const data = useSelector((state) => state.books.books);
+  // const data = useSelector((state) => state.books.books);
+  const data = useSelector((state) => state.fetchdata.items);
+  console.log(data);
+  // const alldata = () => {
+  //   for (let i in data) {
+  //     console.log()
+  //     return (
+  //       <Book
+  //         key={i}
+  //         id={i}
+  //         title={data[i][0].title}
+  //         author={data[i][0].author}
+  //         category={data[i][0].category}
+  //       />
+  //     );
+  //   }
+  // };
+
   return (
     <>
       <div>
-        {data.map((value) => (
-          <Book
-            key={value.item_id}
-            id={value.item_id}
-            title={value.title}
-            author={value.author}
-            category={value.category}
-          />
-        ))}
+        {/* {alldata()} */}
       </div>
       <AddNewBook />
     </>
--- src/redux/books/Fetch.js
+++ src/redux/books/Fetch.js
@@ -1,27 +1,40 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-// const appid="ZtNFhlL0BNvzbTbQF18L";
+
 const appUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ZtNFhlL0BNvzbTbQF18L/books';
 
-const Fetchdata = createAsyncThunk('fetchdata', async () => {
+const fetchData = createAsyncThunk('fetchdata', async () => {
   try {
     const response = await axios.get(appUrl);
     return response.data;
   } catch (error) {
-    throw new Error('data not fetch properly');
+    throw new Error('Data not fetched properly');
   }
 });
 
-const Fetch = createSlice({
+const fetchSlice = createSlice({
   name: 'fetch',
   initialState: {
     items: [],
     isLoading: false,
-    error: undefined,
+    error: null,
   },
   reducers: {},
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchData.rejected, (state) => {
+        state.error = 'Error occurred';
+        state.isLoading = false;
+      });
+  },
 });
 
-export { Fetchdata };
-export default Fetch.reducer;
+export { fetchData };
+export default fetchSlice.reducer;
