bfe5de7567f8697db8a802818e99cdb3d07d6e34
Ankit <ankit26kushwa@gmail.comm>
2023-06-22 11:50:07 +0530

    Add styling in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import { NavLink } from 'react-router-dom';
+import styles from '../CSS/Navbar.module.css';
 
 const Navbar = () => (
-  <nav className="nav">
+  <nav className={styles.nav}>
     <h1>BookStore</h1>
     <ul>
       <li>
-        <NavLink to="/">Books</NavLink>
+        <NavLink to="/" className={styles.books}>Books</NavLink>
       </li>
       <li>
-        <NavLink to="Categories">Categories</NavLink>
+        <NavLink to="Categories" className={styles.categories}>Categories</NavLink>
       </li>
     </ul>
+    <span className={styles.user}><i className="fa-solid fa-user" /></span>
+
   </nav>
 );
 
